bbfad1529b38b27ec68f0f1e8e07567c77475a17
Bedi Omuri <113227370+Bedi06@users.noreply.github.com>
2023-06-26 00:13:08 +0100

    edited app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+function HighScoresTable() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header">      ¡  
       </header>
     </div>
   );
 }
 
-export default App;
+export default HighScoresTable;
