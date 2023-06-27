c5953c7d073b5f3ff3c33d9f89068f608875a7f0
Bedi Omuri <113227370+Bedi06@users.noreply.github.com>
2023-06-27 23:31:07 +0100

    level 3

--- src/HighScoresTable.js
+++ src/HighScoresTable.js
@@ -8,13 +8,16 @@ function HighScoresTable({ country }) {
   if (!scores) {
     return null;
   }
+  const sortedScores = scores.sort((a, b) => b.s - a.s);
+
+
   return (
    <div className="d-flex justify-content-center"> 
         <div className="my-4">
             <h2 className="mb-3 text-center">High Scores : {country} </h2>
                 <table className="table"> 
                     <tbody>
-                        {scores.map((score, index) => (
+                        {sortedScores.map((score, index) => (
                             <PlayerScore key={index} player={score.n} score={score.s} />
                         ))}
                     </tbody>
